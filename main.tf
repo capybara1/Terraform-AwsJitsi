30bccf2fdf5de0d50c4a474a1db3b7d643259eba
capybara1 <39344016+capybara1@users.noreply.github.com>
2020-04-12 15:19:17 +0200

    wip

--- main.tf
+++ main.tf
@@ -75,109 +75,6 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_lb" "default" {
-  name               = "${var.prefix}-Default"
-  load_balancer_type = "application"
-  internal           = false
-  subnets            = aws_subnet.public[*].id
-  security_groups    = [aws_security_group.lb.id]
-  tags = {
-    Name = "${var.prefix}-Default"
-  }
-}
-
-resource "aws_lb_target_group" "default" {
-  name     = "${var.prefix}-Default"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.default.id
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.default.arn
-  port              = "80"
-  protocol          = "HTTP"
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.default.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-FS-1-2-2019-08"
-  certificate_arn   = data.aws_acm_certificate.default.arn
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.default.arn
-  }
-}
-
-resource "aws_route53_record" "cname_record" {
-  zone_id = data.aws_route53_zone.default.zone_id
-  name    = var.service_domain
-  type    = "CNAME"
-  ttl     = "60"
-  records = [aws_lb.default.dns_name]
-}
-
-resource "aws_key_pair" "default" {
-  key_name   = var.key_name
-  public_key = file(var.public_key_path)
-}
-
-resource "aws_security_group" "default" {
-  name        = "${var.prefix}-Default"
-  description = "Controls access from/to instance"
-  vpc_id      = aws_vpc.default.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = local.public_subnet_cidr_blocks
-  }
-
-  ingress {
-    from_port   = 4443
-    to_port     = 4443
-    protocol    = "tcp"
-    cidr_blocks = local.public_subnet_cidr_blocks
-  }
-
-  ingress {
-    from_port   = 10000
-    to_port     = 10000
-    protocol    = "udp"
-    cidr_blocks = local.public_subnet_cidr_blocks
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.prefix}-Default"
-  }
-}
-
 resource "aws_security_group" "lb" {
   name        = "${var.prefix}-ALB"
   description = "Controls access from/to load balancer"
@@ -230,12 +127,155 @@ resource "aws_security_group" "lb" {
   }
 }
 
+resource "aws_lb" "default" {
+  name               = var.prefix
+  load_balancer_type = "network"
+  internal           = false
+  subnets            = aws_subnet.public[*].id
+#  security_groups    = [aws_security_group.lb.id]
+  tags = {
+    Name = var.prefix
+  }
+}
+
+resource "aws_lb_target_group" "http" {
+  name     = "${var.prefix}-Http"
+  port     = 80
+  protocol = "TCP"
+  vpc_id   = aws_vpc.default.id
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.default.arn
+  port              = "80"
+  protocol          = "TCP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.http.arn
+  }
+}
+
+resource "aws_lb_target_group" "https" {
+  name     = "${var.prefix}-Https"
+  port     = 443
+  protocol = "TCP"
+  vpc_id   = aws_vpc.default.id
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.default.arn
+  port              = "443"
+  protocol          = "TCP"
+  #  ssl_policy        = "ELBSecurityPolicy-FS-1-2-2019-08"
+  #  certificate_arn   = data.aws_acm_certificate.default.arn
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.https.arn
+  }
+}
+
+resource "aws_lb_target_group" "videobridge1" {
+  name     = "${var.prefix}-Videobridge1"
+  port     = 4443
+  protocol = "TCP"
+  vpc_id   = aws_vpc.default.id
+}
+
+resource "aws_lb_listener" "videobridge1" {
+  load_balancer_arn = aws_lb.default.arn
+  port              = "4443"
+  protocol          = "TCP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.videobridge1.arn
+  }
+}
+
+resource "aws_lb_target_group" "videobridge2" {
+  name     = "${var.prefix}-Videobridge2"
+  port     = 10000
+  protocol = "UDP"
+  vpc_id   = aws_vpc.default.id
+
+  health_check {
+    protocol = "TCP"
+    port     = 4443
+  }
+}
+
+resource "aws_lb_listener" "videobridge2" {
+  load_balancer_arn = aws_lb.default.arn
+  port              = "10000"
+  protocol          = "UDP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.videobridge2.arn
+  }
+}
+
+resource "aws_key_pair" "default" {
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_security_group" "server" {
+  name        = "${var.prefix}-Server"
+  description = "Controls access from/to Jitsi server instance"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = local.public_subnet_cidr_blocks
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = local.public_subnet_cidr_blocks
+  }
+
+  ingress {
+    from_port   = 4443
+    to_port     = 4443
+    protocol    = "tcp"
+    cidr_blocks = local.public_subnet_cidr_blocks
+  }
+
+  ingress {
+    from_port   = 10000
+    to_port     = 10000
+    protocol    = "udp"
+    cidr_blocks = local.public_subnet_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.prefix}-Server"
+  }
+}
+
 resource "aws_instance" "server" {
   instance_type = var.instance_type
   ami           = var.images[var.aws_region]
   subnet_id     = aws_subnet.public[local.server_subnet_index].id
   vpc_security_group_ids = [
-    aws_security_group.default.id
+    aws_security_group.server.id
   ]
   key_name                    = aws_key_pair.default.id
   associate_public_ip_address = true
@@ -279,7 +319,30 @@ resource "aws_instance" "server" {
 #   instance_id = aws_instance.server.id
 # }
 
-resource "aws_lb_target_group_attachment" "server-default" {
-  target_group_arn = aws_lb_target_group.default.arn
+resource "aws_lb_target_group_attachment" "server-http" {
+  target_group_arn = aws_lb_target_group.http.arn
   target_id        = aws_instance.server.id
+}
+
+resource "aws_lb_target_group_attachment" "server-https" {
+  target_group_arn = aws_lb_target_group.https.arn
+  target_id        = aws_instance.server.id
+}
+
+resource "aws_lb_target_group_attachment" "server-videobridge1" {
+  target_group_arn = aws_lb_target_group.videobridge1.arn
+  target_id        = aws_instance.server.id
+}
+
+resource "aws_lb_target_group_attachment" "server-videobridge2" {
+  target_group_arn = aws_lb_target_group.videobridge2.arn
+  target_id        = aws_instance.server.id
+}
+
+resource "aws_route53_record" "cname_record" {
+  zone_id = data.aws_route53_zone.default.zone_id
+  name    = var.service_domain
+  type    = "CNAME"
+  ttl     = "60"
+  records = [aws_lb.default.dns_name]
 }
