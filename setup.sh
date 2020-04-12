b4d3370d1777ec656072bb80b9328afccb27f158
Jochen <jochen.handels@web.de>
2020-04-12 13:31:38 +0200

    wip

--- setup.sh
+++ setup.sh
@@ -30,8 +30,9 @@ jitsi-videobridge2 jitsi-videobridge/jvb-hostname string $DOMAIN
 jitsi-meet-web-config jitsi-meet/cert-choice string Generate a new self-signed certificate (You will later get a chance to obtain a Let's encrypt certificate)
 jitsi-meet-web-config jitsi-meet/cert-path-crt string /etc/ssl/$DOMAIN.crt
 jitsi-meet-web-config jitsi-meet/cert-path-key string /etc/ssl/$DOMAIN.key
-jitsi-meet-web-config jitsi-videobridge/jvb-hostname string $DOMAIN
 jitsi-meet-web-config jitsi-meet/jvb-hostname string $DOMAIN
+jitsi-meet-turnserver jitsi-meet-turnserver/jvb-hostname string $DOMAIN
+jitsi-meet-web-config jitsi-videobridge/jvb-hostname string $DOMAIN
 EOT
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y jitsi-meet
